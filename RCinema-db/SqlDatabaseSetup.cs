400b93d9ccdd9d105a76602bc9910db25cd44434
RipMyLoven <ripmyloven@gmail.com>
2024-12-05 09:58:04 +0200

    database RCinema-db

---
+++ RCinema-db/SqlDatabaseSetup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace RCinema_db
+{
+    [TestClass()]
+    public class SqlDatabaseSetup
+    {
+        // Этот атрибут и метод необходимы для инициализации тестовой среды
+        [AssemblyInitialize()]
+        public static void InitializeAssembly(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext ctx)
+        {
+            // Настройка тестовой базы данных на основе настроек из конфигурационного файла
+            SqlDatabaseTestClass.TestService.DeployDatabaseProject();
+            SqlDatabaseTestClass.TestService.GenerateData();
+        }
+    }
+}
