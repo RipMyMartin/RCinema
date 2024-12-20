777f77d350661f62526491595e4f14d5f7c6b043
ripmyloven <ripmyloven@gmail.com>
2024-12-20 10:50:22 +0200

    Commit prohect struct

---
+++ RCinema-db/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace RCinema_db.Models
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+        public float Rating { get; set; }
+    }
+}
